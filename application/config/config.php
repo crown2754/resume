97f0c86ed8bdcc360405928ac0e74d10b67cba3c
gary_chen@jinzdm.com <gary_chen@jinzdm.com>
2022-12-05 21:34:00 +0800

    環境初始設定
    環境初始設定

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 
-$config['base_url'] = 'http://127.0.0.1/test/ci/';
+$config['base_url'] = 'http://127.0.0.1/';
 
 /*
 |--------------------------------------------------------------------------
